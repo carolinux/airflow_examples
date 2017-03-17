97d9692b328ecc5314aca97b1cd0c2e5b543870d
carolinux <karolina.alexiou@siroop.ch>
2017-03-17 13:46:18 +0100

    Add xcom

--- siroop/airflow-test/dags/sleeping.py
+++ siroop/airflow-test/dags/sleeping.py
@@ -22,21 +22,18 @@ dag = DAG(
     schedule_interval="0 0 * * * ")
 
 
-def custom_print(**context):
+def run_successful_task(**context):
     '''This is a function that will run within the DAG execution'''
-    time.sleep(1)
+    time.sleep(10)
     logging.info("I am inside the plain python operation")
     logging.info("My context is {}".format(context))
-    logging.info("Env var value available before starting up sched/server is {}".format(os.environ["AIRFLOW_HOME"]))
-    #logging.info("Env var value available after starting up sched/server is {}".format(os.environ["FOO"]))
+    logging.info("Pushing a val thru xcom")
+    context['ti'].xcom_push(key='my_val', value=42)
 
-def raise_exception(**context):
-    time.sleep(10)
-    raise Exception("aaa")
 
 
 def maybe_raise_exception(**context):
-    time.sleep(10)
+    time.sleep(1)
     val = random.randint(1,2)
     if val==1:
         raise Exception("aaa")
@@ -44,26 +41,20 @@ def maybe_raise_exception(**context):
 def run_final_task(**context):
     '''This is a function that will run within the DAG execution'''
     time.sleep(1)
+    context['ti'].xcom_push(key='my_val', value=42)
     logging.info("I am teh final task")
+    val = context['ti'].xcom_pull(key="my_val", task_ids="task_that_suceeds")
+    logging.info("Value read from previous task via xcom: {}".format(val))
     
 
 
 succesful_task = PythonOperator(
     task_id='task_that_succeeds',
     provide_context=True,
-    python_callable=custom_print,
+    python_callable=run_successful_task,
     params={"foo":"bar"},
     dag=dag)
 
-#fail_miserably = PythonOperator(
-#    task_id='fail',
-#    provide_context=True,
-#    retry_delay=timedelta(seconds=20),
-#    python_callable=raise_exception,
-#    on_failure_callback=send_email,
-#    retries=3,
-#    params={"foo":"bar"},
-#    dag=dag)
 
 flaky_task_that_works_some_of_the_time = PythonOperator(
     task_id='flaky_task_that_works_some_of_the_time',
